34d7180587a4d57c983cc3e3c3247076d6831218
Lee <lee.wright@gov.bc.ca>
2023-07-25 16:48:47 -0700

    Fix chart and add version endpoint

---
+++ backend/webapi/Features/VersionInfo/VersionController.cs
@@ -0,0 +1,24 @@
+namespace Pidp.Features.VersionInfo;
+
+using System.Reflection;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Pidp.Infrastructure.Auth;
+using Pidp.Infrastructure.Services;
+
+[Route("api/[controller]")]
+[Authorize(Policy = Policies.AnyPartyIdentityProvider)]
+
+public class VersionController : PidpControllerBase
+{
+
+    public VersionController(IPidpAuthorizationService authService) : base(authService) { }
+
+    [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public string Index()
+    {
+        var version = Assembly.GetEntryAssembly().GetCustomAttribute<AssemblyInformationalVersionAttribute>().InformationalVersion;
+        return $"{version}";
+    }
+}
