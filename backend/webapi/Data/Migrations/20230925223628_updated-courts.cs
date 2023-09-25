ecc6669c7841d07e5a88a4e076c7ec48fc6405eb
Lee <lee.wright@gov.bc.ca>
2023-09-25 15:50:47 -0700

    Update court locations

---
+++ backend/webapi/Data/Migrations/20230925223628_updated-courts.cs
@@ -0,0 +1,500 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Pidp.Data.Migrations
+{
+  public partial class updatedcourts : Migration
+  {
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+      migrationBuilder.DeleteData(
+          table: "CourtLocation",
+          keyColumn: "Code",
+          keyValue: "1111");
+
+      migrationBuilder.DeleteData(
+          table: "CourtLocation",
+          keyColumn: "Code",
+          keyValue: "1171");
+
+      migrationBuilder.DeleteData(
+          table: "CourtLocation",
+          keyColumn: "Code",
+          keyValue: "2035");
+
+      migrationBuilder.DeleteData(
+          table: "CourtLocation",
+          keyColumn: "Code",
+          keyValue: "2049");
+
+      migrationBuilder.DeleteData(
+          table: "CourtLocation",
+          keyColumn: "Code",
+          keyValue: "204A");
+
+      migrationBuilder.DeleteData(
+          table: "CourtLocation",
+          keyColumn: "Code",
+          keyValue: "204B");
+
+      migrationBuilder.DeleteData(
+          table: "CourtLocation",
+          keyColumn: "Code",
+          keyValue: "2051");
+
+      migrationBuilder.DeleteData(
+          table: "CourtLocation",
+          keyColumn: "Code",
+          keyValue: "3511");
+
+      migrationBuilder.DeleteData(
+          table: "CourtLocation",
+          keyColumn: "Code",
+          keyValue: "3535");
+
+      migrationBuilder.DeleteData(
+          table: "CourtLocation",
+          keyColumn: "Code",
+          keyValue: "3541");
+
+      migrationBuilder.DeleteData(
+          table: "CourtLocation",
+          keyColumn: "Code",
+          keyValue: "3545");
+
+      migrationBuilder.DeleteData(
+          table: "CourtLocation",
+          keyColumn: "Code",
+          keyValue: "3551");
+
+      migrationBuilder.DeleteData(
+          table: "CourtLocation",
+          keyColumn: "Code",
+          keyValue: "3561");
+
+      migrationBuilder.DeleteData(
+          table: "CourtLocation",
+          keyColumn: "Code",
+          keyValue: "3571");
+
+      migrationBuilder.DeleteData(
+          table: "CourtLocation",
+          keyColumn: "Code",
+          keyValue: "3587");
+
+      migrationBuilder.DeleteData(
+          table: "CourtLocation",
+          keyColumn: "Code",
+          keyValue: "3588");
+
+      migrationBuilder.DeleteData(
+          table: "CourtLocation",
+          keyColumn: "Code",
+          keyValue: "4671");
+
+      migrationBuilder.DeleteData(
+          table: "CourtLocation",
+          keyColumn: "Code",
+          keyValue: "4691");
+
+      migrationBuilder.DeleteData(
+          table: "CourtLocation",
+          keyColumn: "Code",
+          keyValue: "4811");
+
+      migrationBuilder.DeleteData(
+          table: "CourtLocation",
+          keyColumn: "Code",
+          keyValue: "4831");
+
+      migrationBuilder.DeleteData(
+          table: "CourtLocation",
+          keyColumn: "Code",
+          keyValue: "4881");
+
+      migrationBuilder.DeleteData(
+          table: "CourtLocation",
+          keyColumn: "Code",
+          keyValue: "4899");
+
+      migrationBuilder.DeleteData(
+          table: "CourtLocation",
+          keyColumn: "Code",
+          keyValue: "5671");
+
+      migrationBuilder.DeleteData(
+          table: "CourtLocation",
+          keyColumn: "Code",
+          keyValue: "5801");
+
+      migrationBuilder.DeleteData(
+          table: "CourtLocation",
+          keyColumn: "Code",
+          keyValue: "ADJU");
+
+      migrationBuilder.DeleteData(
+          table: "CourtLocation",
+          keyColumn: "Code",
+          keyValue: "CCC");
+
+      migrationBuilder.DeleteData(
+          table: "CourtLocation",
+          keyColumn: "Code",
+          keyValue: "CKH");
+
+      migrationBuilder.DeleteData(
+          table: "CourtLocation",
+          keyColumn: "Code",
+          keyValue: "CVCC");
+
+      migrationBuilder.DeleteData(
+          table: "CourtLocation",
+          keyColumn: "Code",
+          keyValue: "EHC");
+
+      migrationBuilder.DeleteData(
+          table: "CourtLocation",
+          keyColumn: "Code",
+          keyValue: "ESCC");
+
+      migrationBuilder.DeleteData(
+          table: "CourtLocation",
+          keyColumn: "Code",
+          keyValue: "KPAC");
+
+      migrationBuilder.DeleteData(
+          table: "CourtLocation",
+          keyColumn: "Code",
+          keyValue: "KPU");
+
+      migrationBuilder.DeleteData(
+          table: "CourtLocation",
+          keyColumn: "Code",
+          keyValue: "KSS");
+
+      migrationBuilder.DeleteData(
+          table: "CourtLocation",
+          keyColumn: "Code",
+          keyValue: "LDRC");
+
+      migrationBuilder.DeleteData(
+          table: "CourtLocation",
+          keyColumn: "Code",
+          keyValue: "NCT");
+
+      migrationBuilder.DeleteData(
+          table: "CourtLocation",
+          keyColumn: "Code",
+          keyValue: "NDSC");
+
+      migrationBuilder.DeleteData(
+          table: "CourtLocation",
+          keyColumn: "Code",
+          keyValue: "PSC");
+
+      migrationBuilder.DeleteData(
+          table: "CourtLocation",
+          keyColumn: "Code",
+          keyValue: "RSS");
+
+      migrationBuilder.DeleteData(
+          table: "CourtLocation",
+          keyColumn: "Code",
+          keyValue: "SCCB");
+
+      migrationBuilder.DeleteData(
+          table: "CourtLocation",
+          keyColumn: "Code",
+          keyValue: "SCCH");
+
+      migrationBuilder.DeleteData(
+          table: "CourtLocation",
+          keyColumn: "Code",
+          keyValue: "SCCO");
+
+      migrationBuilder.DeleteData(
+          table: "CourtLocation",
+          keyColumn: "Code",
+          keyValue: "SCCR");
+
+      migrationBuilder.DeleteData(
+          table: "CourtLocation",
+          keyColumn: "Code",
+          keyValue: "SCCS");
+
+      migrationBuilder.DeleteData(
+          table: "CourtLocation",
+          keyColumn: "Code",
+          keyValue: "SCDC");
+
+      migrationBuilder.DeleteData(
+          table: "CourtLocation",
+          keyColumn: "Code",
+          keyValue: "SCDU");
+
+      migrationBuilder.DeleteData(
+          table: "CourtLocation",
+          keyColumn: "Code",
+          keyValue: "SCFJ");
+
+      migrationBuilder.DeleteData(
+          table: "CourtLocation",
+          keyColumn: "Code",
+          keyValue: "SCFN");
+
+      migrationBuilder.DeleteData(
+          table: "CourtLocation",
+          keyColumn: "Code",
+          keyValue: "SCGD");
+
+      migrationBuilder.DeleteData(
+          table: "CourtLocation",
+          keyColumn: "Code",
+          keyValue: "SCIN");
+
+      migrationBuilder.DeleteData(
+          table: "CourtLocation",
+          keyColumn: "Code",
+          keyValue: "SCKA");
+
+      migrationBuilder.DeleteData(
+          table: "CourtLocation",
+          keyColumn: "Code",
+          keyValue: "SCKE");
+
+      migrationBuilder.DeleteData(
+          table: "CourtLocation",
+          keyColumn: "Code",
+          keyValue: "SCMA");
+
+      migrationBuilder.DeleteData(
+          table: "CourtLocation",
+          keyColumn: "Code",
+          keyValue: "SCNA");
+
+      migrationBuilder.DeleteData(
+          table: "CourtLocation",
+          keyColumn: "Code",
+          keyValue: "SCNE");
+
+      migrationBuilder.DeleteData(
+          table: "CourtLocation",
+          keyColumn: "Code",
+          keyValue: "SCNW");
+
+      migrationBuilder.DeleteData(
+          table: "CourtLocation",
+          keyColumn: "Code",
+          keyValue: "SCPA");
+
+      migrationBuilder.DeleteData(
+          table: "CourtLocation",
+          keyColumn: "Code",
+          keyValue: "SCPE");
+
+      migrationBuilder.DeleteData(
+          table: "CourtLocation",
+          keyColumn: "Code",
+          keyValue: "SCPG");
+
+      migrationBuilder.DeleteData(
+          table: "CourtLocation",
+          keyColumn: "Code",
+          keyValue: "SCPR");
+
+      migrationBuilder.DeleteData(
+          table: "CourtLocation",
+          keyColumn: "Code",
+          keyValue: "SCQU");
+
+      migrationBuilder.DeleteData(
+          table: "CourtLocation",
+          keyColumn: "Code",
+          keyValue: "SCRE");
+
+      migrationBuilder.DeleteData(
+          table: "CourtLocation",
+          keyColumn: "Code",
+          keyValue: "SCRO");
+
+      migrationBuilder.DeleteData(
+          table: "CourtLocation",
+          keyColumn: "Code",
+          keyValue: "SCRP");
+
+      migrationBuilder.DeleteData(
+          table: "CourtLocation",
+          keyColumn: "Code",
+          keyValue: "SCSA");
+
+      migrationBuilder.DeleteData(
+          table: "CourtLocation",
+          keyColumn: "Code",
+          keyValue: "SCSM");
+
+      migrationBuilder.DeleteData(
+          table: "CourtLocation",
+          keyColumn: "Code",
+          keyValue: "SCTE");
+
+      migrationBuilder.DeleteData(
+          table: "CourtLocation",
+          keyColumn: "Code",
+          keyValue: "SCVA");
+
+      migrationBuilder.DeleteData(
+          table: "CourtLocation",
+          keyColumn: "Code",
+          keyValue: "SCVE");
+
+      migrationBuilder.DeleteData(
+          table: "CourtLocation",
+          keyColumn: "Code",
+          keyValue: "SCVI");
+
+      migrationBuilder.DeleteData(
+          table: "CourtLocation",
+          keyColumn: "Code",
+          keyValue: "SCWL");
+
+      migrationBuilder.DeleteData(
+          table: "CourtLocation",
+          keyColumn: "Code",
+          keyValue: "SSS");
+
+      migrationBuilder.DeleteData(
+          table: "CourtLocation",
+          keyColumn: "Code",
+          keyValue: "TSP");
+
+      migrationBuilder.DeleteData(
+          table: "CourtLocation",
+          keyColumn: "Code",
+          keyValue: "UFV");
+
+      migrationBuilder.DeleteData(
+          table: "CourtLocation",
+          keyColumn: "Code",
+          keyValue: "UVIC");
+
+      migrationBuilder.DeleteData(
+          table: "CourtLocation",
+          keyColumn: "Code",
+          keyValue: "VAJA");
+
+      migrationBuilder.DeleteData(
+          table: "CourtLocation",
+          keyColumn: "Code",
+          keyValue: "VICA");
+
+      migrationBuilder.DeleteData(
+          table: "CourtLocation",
+          keyColumn: "Code",
+          keyValue: "VT");
+
+      migrationBuilder.DeleteData(
+          table: "CourtLocation",
+          keyColumn: "Code",
+          keyValue: "WLEH");
+
+      migrationBuilder.InsertData(
+          table: "CourtLocation",
+          columns: new[] { "Code", "Active", "Name", "Staffed" },
+          values: new object[] { "ï»¿3561", true, "Abbotsford Law Courts", true });
+    }
+
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+      migrationBuilder.DeleteData(
+          table: "CourtLocation",
+          keyColumn: "Code",
+          keyValue: "ï»¿3561");
+
+      migrationBuilder.InsertData(
+          table: "CourtLocation",
+          columns: new[] { "Code", "Active", "Name", "Staffed" },
+          values: new object[,]
+          {
+                    { "1111", true, "Parksville Provincial Court", true },
+                    { "1171", true, "Tahsis Provincial Court", true },
+                    { "2035", true, "Squamish Provincial Court", true },
+                    { "2049", true, "Violation Ticket Centre", true },
+                    { "204A", true, "Vancouver Provincial Court - After Hours/Justice Centre Only", true },
+                    { "204B", true, "Vancouver Provincial Court - ALTERNATE After Hours Fax Number", true },
+                    { "2051", true, "West Vancouver Provincial Court", true },
+                    { "3511", true, "Burnaby Provincial Court", true },
+                    { "3535", true, "Delta Provincial Court", true },
+                    { "3541", true, "Hope Provincial Court", true },
+                    { "3545", true, "Langley Provincial Court", true },
+                    { "3551", true, "Maple Ridge Provincial Court", true },
+                    { "3561", true, "Abbotsford Law Courts", true },
+                    { "3571", true, "Mission Provincial Courts", true },
+                    { "3587", true, "Surrey Provincial Weekend - Admin Department", true },
+                    { "3588", true, "Surrey First Nations Court", true },
+                    { "4671", true, "Ashcroft Provincial Court", true },
+                    { "4691", true, "Chase Provincial Court", true },
+                    { "4811", true, "Kimberley Provincial Court", true },
+                    { "4831", true, "Lytton Provincial Court", true },
+                    { "4881", true, "Oliver Law Courts", true },
+                    { "4899", true, "Sheriffs Escort Centre", true },
+                    { "5671", true, "Alexis Creek Provincial Court", true },
+                    { "5801", true, "Hudson's Hope Provincial Court", true },
+                    { "ADJU", true, "Adjudicator Listing", true },
+                    { "CCC", true, "Chilliwack Cultural Centre", true },
+                    { "CKH", true, "Coast Kamloops Hotel and Conference Centre", true },
+                    { "CVCC", true, "Cowichan Community Centre", true },
+                    { "EHC", true, "Evergreen Hall", true },
+                    { "ESCC", true, "Erwin Stege Community Centre", true },
+                    { "KPAC", true, "Kiwanis Performing Arts Centre (Dawson Creek)", true },
+                    { "KPU", true, "Kwantlen Polytechnic University", true },
+                    { "KSS", true, "Kitsilano Secondary School", true },
+                    { "LDRC", true, "Lillooet District Recreational Centre", true },
+                    { "NCT", true, "Nelson Capitol Theatre", true },
+                    { "NDSC", true, "Nakusp and District Sports Complex", true },
+                    { "PSC", true, "Penticton Seniors Centre", true },
+                    { "RSS", true, "Riverside Secondary School", true },
+                    { "SCCB", true, "Supreme Court Scheduling - Cranbrook", true },
+                    { "SCCH", true, "Supreme Court Scheduling - Chilliwack", true },
+                    { "SCCO", true, "Supreme Court Scheduling - Courtenay", true },
+                    { "SCCR", true, "Supreme Court Scheduling - Campbell River", true },
+                    { "SCCS", true, "Supreme Court Scheduling - Creston", true },
+                    { "SCDC", true, "Supreme Court Scheduling - Dawson Creek", true },
+                    { "SCDU", true, "Supreme Court Scheduling - Duncan", true },
+                    { "SCFJ", true, "Supreme Court Scheduling - Fort St. John", true },
+                    { "SCFN", true, "Supreme Court Scheduling - Fort Nelson", true },
+                    { "SCGD", true, "Supreme Court Scheduling - Golden", true },
+                    { "SCIN", true, "Supreme Court Scheduling - Invermere", true },
+                    { "SCKA", true, "Supreme Court Scheduling - Kamloops", true },
+                    { "SCKE", true, "Supreme Court Scheduling - Kelowna", true },
+                    { "SCMA", true, "Supreme Court Scheduling - Massett", true },
+                    { "SCNA", true, "Supreme Court Scheduling - Nanaimo", true },
+                    { "SCNE", true, "Supreme Court Scheduling - Nelson", true },
+                    { "SCNW", true, "Supreme Court Scheduling - New Westminster", true },
+                    { "SCPA", true, "Supreme Court Scheduling - Port Alberni", true },
+                    { "SCPE", true, "Supreme Court Scheduling - Penticton", true },
+                    { "SCPG", true, "Supreme Court Scheduling - Prince George", true },
+                    { "SCPR", true, "Supreme Court Scheduling - Powell River", true },
+                    { "SCQU", true, "Supreme Court Scheduling - Quesnel", true },
+                    { "SCRE", true, "Supreme Court Scheduling - Revelstoke", true },
+                    { "SCRO", true, "Supreme Court Scheduling - Rossland", true },
+                    { "SCRP", true, "Supreme Court Scheduling - Prince Rupert", true },
+                    { "SCSA", true, "Supreme Court Scheduling - Salmon Arm", true },
+                    { "SCSM", true, "Supreme Court Scheduling - Smithers", true },
+                    { "SCTE", true, "Supreme Court Scheduling - Terrace", true },
+                    { "SCVA", true, "Supreme Court Scheduling - Vancouver", true },
+                    { "SCVE", true, "Supreme Court Scheduling - Vernon", true },
+                    { "SCVI", true, "Supreme Court Scheduling - Victoria", true },
+                    { "SCWL", true, "Supreme Court Scheduling - Williams Lake", true },
+                    { "SSS", true, "Sutherland Secondary School", true },
+                    { "TSP", true, "Terrace Sportsplex", true },
+                    { "UFV", true, "University of Fraser Valley", true },
+                    { "UVIC", true, "University of Victoria", true },
+                    { "VAJA", true, "Vancouver Jail - Alternate Fax Number", true },
+                    { "VICA", true, "Victoria - Accounting Section", true },
+                    { "VT", true, "Vancouver Training", true },
+                    { "WLEH", true, "Williams Lake Elks Hall", true }
+          });
+    }
+  }
+}
