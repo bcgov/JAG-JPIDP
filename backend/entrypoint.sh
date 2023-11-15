a676fa9cc90fa63332b710737d03c5bd502c1963
Lee <lee.wright@gov.bc.ca>
2023-11-15 15:15:57 -0800

    Chart updates

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
-
 # -e          : causes script to fail if any command below has non-zero exit status
 # -u          : a reference to any variable you haven't previously defined cause immediate exit
 # -o pipefail : prevents errors in a pipeline from being masked. If any command in a pipeline fails, 
 #               that return code will be used as the return code of the whole pipeline.
-set -euo pipefail 
-
+set -euo pipefail
 # shellcheck disable=SC1091
-
 VAULT_SECRETS_DIR=/vault/secrets
 
 if [ -d ${VAULT_SECRETS_DIR} ]; then
