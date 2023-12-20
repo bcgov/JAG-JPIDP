8d5fb31b57a11d3c865b5a0aed069bd71547d7b5
Lee <lee.wright@gov.bc.ca>
2023-12-20 12:12:47 -0800

    Fix value fields

--- backend/common/Models/EDT/EdtField.cs
+++ backend/common/Models/EDT/EdtField.cs
@@ -4,6 +4,12 @@ public class EdtField
 {
     public string? Name { get; set; }
     public int? Id { get; set; }
-    public string? Value { get; set; }
+    public List<ValueField> Value { get; set; }
 }
 
+public class ValueField
+{
+    public int Id { get; set; }
+    public string? Value { get; set; } = String.Empty;
+    public string? Name { get;set; } = String.Empty; 
+}
