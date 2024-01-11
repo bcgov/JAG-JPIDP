6cd31740ad2fb677b138743ec8ffb880e085125b
Lee <lee.wright@gov.bc.ca>
2024-01-11 15:10:44 -0800

    Permit multiple groups for counsel/ooc

--- backend/edt.disclosure/ServiceEvents/UserAccountCreation/Handler/BaseProvisioningHandler.cs
+++ backend/edt.disclosure/ServiceEvents/UserAccountCreation/Handler/BaseProvisioningHandler.cs
@@ -100,7 +100,7 @@ public abstract class BaseProvisioningHandler
         var user = await this.edtClient.GetUser(accessRequestModel.Key!) ?? throw new EdtDisclosureServiceException($"User was not found {accessRequestModel.Key}");
 
         var caseGroups = (accessRequestModel.OrganizationType == this.configuration.EdtClient.OutOfCustodyOrgType) ? this.configuration.EdtClient.OutOfCustodyCaseGroups : this.configuration.EdtClient.DefenceCaseGroups;
-        var caseGroupArr = caseGroups.Split(",", StringSplitOptions.TrimEntries);
+        var caseGroupArr = caseGroups.Split(",", StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
 
         if (caseGroupArr.Any())
         {
