6267cd8a99510703cf670ff92c221bd7bcba0b88
Chandragupthan <Sujitha.Chandragupthan@gov.bc.ca>
2024-05-13 12:40:15 -0700

    new changes

--- auth/extensions/services/src/main/java/com/github/bcgov/keycloak/authenticators/csnumber/CSNumberAuthenticator.java
+++ auth/extensions/services/src/main/java/com/github/bcgov/keycloak/authenticators/csnumber/CSNumberAuthenticator.java
@@ -2,19 +2,15 @@ package com.github.bcgov.keycloak.authenticators.csnumber;
 
 import jakarta.ws.rs.core.MultivaluedMap;
 import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.UriBuilder;
 import org.keycloak.authentication.AuthenticationFlowContext;
 import org.keycloak.authentication.AuthenticationFlowError;
 import org.keycloak.authentication.Authenticator;
 import org.keycloak.forms.login.LoginFormsProvider;
 import org.keycloak.forms.login.freemarker.FreeMarkerLoginFormsProvider;
-import org.keycloak.models.AuthenticatorConfigModel;
-import org.keycloak.models.KeycloakSession;
-import org.keycloak.models.RealmModel;
-import org.keycloak.models.UserModel;
+import org.keycloak.models.*;
 import org.keycloak.models.utils.FormMessage;
 import org.keycloak.sessions.AuthenticationSessionModel;
-
-import java.util.Map;
 
 /**
  * Author lee.wright@gov.bc.ca
@@ -25,44 +21,60 @@ public class CSNumberAuthenticator implements Authenticator {
   private static final String ERROR_MESSAGE = "csnumber.result.error";
   private static final String TEMPLATE = "csnumberform.ftl";
 
+
   @Override
   public void authenticate(AuthenticationFlowContext context) {
-    //Response response = prepareCSNumber(context, null);
-    Response response = context.form().createForm(TEMPLATE);
-    context.challenge(response);
+    System.out.println("authenticate");
+      Response response = context.form().createForm(TEMPLATE);
+      context.challenge(response);
   }
 
-  @Override
   public void action(AuthenticationFlowContext context) {
-    AuthenticationSessionModel authSession = context.getAuthenticationSession();
-    String mathResult = authSession.getAuthNote(RESULT_FIELD);
-    int result = Integer.parseInt(mathResult);
+ System.out.println("action");
+      MultivaluedMap<String, String> formParameters = context.getHttpRequest().getDecodedFormParameters();
+      String enteredResult = formParameters.getFirst("login");
 
-    MultivaluedMap<String, String> formParameters = context.getHttpRequest().getDecodedFormParameters();
-    String enteredResult = formParameters.getFirst("result");
-    int enteredResultNumber = Integer.parseInt(enteredResult);
+      if (enteredResult != null) {
+          StringBuilder enteredCSNumber = new StringBuilder();
 
-    if (result == enteredResultNumber) {
-      context.success();
-    } else {
-      FormMessage errorMessage = new FormMessage(ERROR_MESSAGE);
-      Response response = prepareCSNumber(context, errorMessage);
-      context.failureChallenge(AuthenticationFlowError.INVALID_CREDENTIALS, response);
-    }
+          for ( int i = 1; i < formParameters.size(); i++){
+              String key = "cs_num_"+i;
+              String value = formParameters.get(key).get(0);
+              enteredCSNumber.append(value);
+          }
+
+          if(enteredCSNumber.isEmpty()) {
+              FormMessage errorMessage = new FormMessage(ERROR_MESSAGE);
+              Response response = prepareCSNumber(context, errorMessage);
+              context.failureChallenge(AuthenticationFlowError.INVALID_CREDENTIALS, response);
+          }
+          context.resetFlow();
+
+          int result = Integer.parseInt(enteredCSNumber.toString());
+          System.out.println(result);
+          System.out.println(context.getRefreshExecutionUrl());
+
+          UriBuilder loginUriBuilder = UriBuilder.fromUri(context.getRefreshExecutionUrl())
+                  .queryParam("cs_number", result);
+          Response response = Response.seeOther(loginUriBuilder.build()).build();
+          context.forceChallenge(response);
+
+          System.out.println(context.getRefreshExecutionUrl());
+          return;
+      }
   }
 
+  private static int getRandomNumber(int lowerBound, int upperBound) {
+    return (int) ((Math.random() * (upperBound - lowerBound)) + lowerBound);
+  }
   private static Response prepareCSNumber(AuthenticationFlowContext context, FormMessage errorMessage) {
-    AuthenticatorConfigModel authenticatorConfig = context.getAuthenticatorConfig();
+    //AuthenticatorConfigModel authenticatorConfig = context.getAuthenticatorConfig();
     //Map<String, String> config = authenticatorConfig.getConfig();
 
     AuthenticationSessionModel authSession = context.getAuthenticationSession();
     authSession.setAuthNote(RESULT_FIELD, Integer.toString(12345678));
     LoginFormsProvider formsProvider = new FreeMarkerLoginFormsProvider(context.getSession());
     return formsProvider.createForm(TEMPLATE);
-  }
-
-  private static int getRandomNumber(int lowerBound, int upperBound) {
-    return (int) ((Math.random() * (upperBound - lowerBound)) + lowerBound);
   }
 
   @Override
--- auth/extensions/services/src/main/resources/theme-resources/templates/csnumberform.ftl
+++ auth/extensions/services/src/main/resources/theme-resources/templates/csnumberform.ftl
@@ -1,28 +1,35 @@
 <#import "template.ftl" as layout>
 <@layout.registrationLayout; section>
+<#--    <link src="${url.resourcesPath}/../diam-corrections/css/styles-20221128.css" rel="stylesheet" defer> </link>-->
+<#--    <link src="${url.resourcesPath}/../diam-corrections/login/resources/css/styles-20221128.css" rel="stylesheet" ></link>-->
     <#if section = "header">
       <h1>BC Corrections Service Login</h1>
       <h2>Please enter your Corrections Service (CS) Number</h2><#elseif section = "form">
-<#--    < section = "form">-->
         <div id="kc-form">
             <div id="kc-form-wrapper">
-            		<h3>
-            												<span>${msg("csNumber.title")}</span>
-            										</h3>
-   <form id="kc-form-login" action="${url.loginAction}" method="post">
-            <input class="digit" id="cs_num_1" type="number" min="0" max="9" maxlength="1" autofocus></input>
-            <input class="digit" id="cs_num_2" type="number" min="0" max="9" maxlength="1"></input>
-            <input class="digit" id="cs_num_3" type="number" min="0" max="9" maxlength="1"></input>
-            <input class="digit" id="cs_num_4" type="number" min="0" max="9" maxlength="1"></input>
-            <input class="digit" id="cs_num_5" type="number" min="0" max="9" maxlength="1"></input>
-            <input class="digit" id="cs_num_6" type="number" min="0" max="9" maxlength="1"></input>
-            <input class="digit" id="cs_num_7" type="number" min="0" max="9" maxlength="1"></input>
-            <input class="digit" id="cs_num_8" type="number" min="0" max="9" maxlength="1"></input>
-            <div id="kc-form-buttons" class="${properties.kcFormGroupClass!}">
-                <input tabindex="7" class="${properties.kcButtonClass!} ${properties.kcButtonPrimaryClass!} ${properties.kcButtonBlockClass!} ${properties.kcButtonLargeClass!}" name="login" id="kc-login" type="submit" value="${msg("doLogIn")}"/>
-            </div>
-        </form>
+            	<h3>
+            		<span>${msg("csNumber.title")}</span>
+            	</h3>
+                <form class="${properties.kcFormClass!}" id="kc-form-login" action="${url.loginAction}" method="post">
+    
+                    <div class="${properties.kcFormGroupClass!}">
+                        <input class="${properties.kcInputClass!}" id="cs_num_1" type="number" name="cs_num_1" min="0" max="9" maxlength="1" autofocus/>
+                        <input class="${properties.kcInputClass!}" id="cs_num_2" type="number" name="cs_num_2" min="0" max="9" maxlength="1" />
+                        <input class="${properties.kcInputClass!}" id="cs_num_3" type="number" name="cs_num_3" min="0" max="9" maxlength="1" />
+                        <input class="${properties.kcInputClass!}" id="cs_num_4" type="number" name="cs_num_4" min="0" max="9" maxlength="1" />
+                        <input class="${properties.kcInputClass!}" id="cs_num_5" type="number" name="cs_num_5" min="0" max="9" maxlength="1" />
+                        <input class="${properties.kcInputClass!}" id="cs_num_6" type="number" name="cs_num_6" min="0" max="9" maxlength="1" />
+                        <input class="${properties.kcInputClass!}" id="cs_num_7" type="number" name="cs_num_7" min="0" max="9" maxlength="1" />
+                        <input class="${properties.kcInputClass!}" id="cs_num_8" type="number" name="cs_num_8" min="0" max="9" maxlength="1" />
+                    </div>
+                    <div id="kc-form-buttons" class="${properties.kcFormGroupClass!}">
+                        <input tabindex="7" class="${properties.kcButtonClass!} ${properties.kcButtonPrimaryClass!} ${properties.kcButtonBlockClass!} ${properties.kcButtonLargeClass!}" name="login" id="kc-login" type="submit" value="${msg("doLogIn")}"/>
+                    </div>
+                </form>
             </div>
         </div>
     </#if>
+    <script src="${url.resourcesPath}/../diam-corrections/js/script-20221128.js" defer></script>
+    <style type="text/css"> <#include "css/styles-20221128.css"> </style>
+    <style type="text/css"> <#include "css/bcsans-20221128.css"> </style>
 </@layout.registrationLayout>
