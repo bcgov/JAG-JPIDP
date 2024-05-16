07f9a77cdd900641d9dfa07afec04fadb242c4dd
Lee Wright <258036@NTTDATA.COM>
2024-05-16 13:24:32 -0700

    moved

--- backend/edt.notifications/Configuration/PublisherOptions.cs
+++
@@ -1,9 +0,0 @@
-namespace edt.notifications.Configuration;
-
-public class PublisherOptions
-{
-    public const string Publisher = "Publisher";
-
-    public string SNSTarget { get; set; }
-
-}
--- backend/edt.notifications/Configuration/SubscriberOptions.cs
+++
@@ -1,8 +0,0 @@
-namespace edt.notifications.Configuration;
-
-public class SubscriberOptions
-{
-    public const string Subscriber = "Subscriber";
-
-    public string SQSUrl { get; set; }
-}
