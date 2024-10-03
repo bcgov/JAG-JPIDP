8fba1f3639b8ce65421ca4a08b15b777f4f8f709
leewrigh <lee.wright@gov.bc.ca>
2024-10-03 15:07:45 -0700

    Case processing changes

--- backend/edt.casemanagement/Models/UserCaseGroup.cs
+++ backend/edt.casemanagement/Models/UserCaseGroup.cs
@@ -4,8 +4,8 @@ public class UserCaseGroup
 {
     public string UserId { get; set; } = string.Empty;
     public int GroupUserId { get; set; }
-
     public string GroupName { get; set; } = string.Empty;
-
     public int GroupId { get; set; }
+
+    public override string ToString() => $"UserId: {this.UserId}, GroupUserId: {this.GroupUserId}, GroupName: {this.GroupName}, GroupId: {this.GroupId}";
 }
