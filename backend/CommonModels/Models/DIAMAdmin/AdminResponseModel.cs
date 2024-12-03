4c807a640e15354f3bd1a78d61e792db4bfb8b22
leewrigh <lee.wright@gov.bc.ca>
2024-12-03 13:15:41 -0800

    New models

--- backend/CommonModels/Models/DIAMAdmin/AdminResponseModel.cs
+++ backend/CommonModels/Models/DIAMAdmin/AdminResponseModel.cs
@@ -9,6 +9,7 @@ public class AdminResponseModel
     public required Guid RequestId { get; set; }
     public DateTime RequestProcessDateTime { get; set; }
     public required string Hostname { get; set; }
+    public AdminCommandSet RequestType { get; set; }
     public string? Errors { get; set; }
     public bool Success { get; set; }
     public Dictionary<string, string> ResponseData { get; set; } = [];
